d55e2fc2e783ba713c4384d29ba719efee2dda2a
rajankit <ankit.raj.min14@itbhu.ac.in>
2018-08-23 18:42:48 +0530

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,15 +14,33 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('New User coneected');
-   
+
+    socket.emit('newMessage',{
+      from: 'admin',
+      text: 'Welcome to the chat App',
+      createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage',{
+      from: 'Admin',
+      text: 'New User Joined',
+      createdAt: new Date().getTime()
+    });
+
+
+
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
-
-        io.emit('newMessage',{
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        });
+        // io.emit('newMessage',{
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
+        // socket.broadcast.emit('newMessage',{
+        //   from: message.from,
+        //   text: message.text,
+        //   createdAt: new Date().getTime()
+        // });
     });
 
     socket.on('disconnect', () => {
