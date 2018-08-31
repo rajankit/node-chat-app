4d4f040a22053d2cee3cc1ac20ab7f4379db9b2f
rajankit <ankit.raj.min14@itbhu.ac.in>
2018-08-31 16:17:07 +0530

    Add mustacheJS for message template

--- public/js/index.js
+++ public/js/index.js
@@ -13,31 +13,26 @@ socket.on('disconnect', function () {
 // Listen to the event created by server with NAME - newMessage
 
 socket.on('newMessage', function (message) {
-    var formattedTime = moment(message.createdAt).format('h:mm:ss a');
-
-    var li = jQuery('<li></li>');
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);
-
-    jQuery('#messages').append(li);
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+    var template = jQuery('#message-template').html();
+    var html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
+    jQuery('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function (message) {
-    var li = jQuery('<li></li>');
-    var a = jQuery('<a target = "_blank">My current location</a>');
-    var formattedTime = moment(message.createdAt).format('h:mm:ss a');
-    li.text(`${message.from} ${formattedTime}: `);
-    a.attr('href', message.url);
-    li.append(a);
-
-    jQuery('#messages').append(li);
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+    var template = jQuery('#location-message-template').html();
+    var html = Mustache.render(template, {
+        url: message.url,
+        from: message.from,
+        createdAt: formattedTime
+    });
+    jQuery('#messages').append(html);
 });
-
-// socket.emit('createMessage',{
-//     from: 'Frank',
-//     text: 'Hi'
-// }, function (data) {
-//     console.log('Got It',data);
-// });
 
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
--- server/server.js
+++ server/server.js
@@ -20,8 +20,6 @@ io.on('connection', (socket) => {
 
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User joined'));
 
-
-
     socket.on('createMessage', (message,callback) => {
         console.log('createMessage', message);
         io.emit('newMessage',generateMessage(message.from, message.text));
