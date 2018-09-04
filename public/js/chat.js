9dd80be01dbe57f8462a4b09ca511d617986080c
rajankit <ankit.raj.min14@itbhu.ac.in>
2018-09-04 19:35:50 +0530

    Send Messages to only people to only same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -74,9 +74,8 @@ jQuery('#message-form').on('submit', function (e) {
 
 
     var messageTextbox = jQuery('[name=message]');
-
+    //var params = jQuery.deparam(window.location.search);
     socket.emit('createMessage', {
-        from : 'User',
         text : messageTextbox.val()
     },function () {
         messageTextbox.val('');
--- server/server.js
+++ server/server.js
@@ -32,15 +32,22 @@ io.on('connection', (socket) => {
         socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined.`));
         callback();
     });
+
+
     socket.on('createMessage', (message,callback) => {
-        console.log('createMessage', message);
-        io.emit('newMessage',generateMessage(message.from, message.text));
-        
+        var user = users.getUser(socket.id);
+        if(user && isRealString(message.text)){
+            io.to(user.room).emit('newMessage',generateMessage(user.name, message.text));
+        }
         callback();
     });
 
     socket.on('createLocationMessage',(coords) => {
-        io.emit('newLocationMessage',generateLocationMessage('Admin',coords.latitude,coords.longitude));
+        var user = users.getUser(socket.id);
+        // console.log(user);
+        if(user){
+            io.to(user.room).emit('newLocationMessage',generateLocationMessage(user.name,coords.latitude,coords.longitude));
+        }
     });
 
     socket.on('disconnect', () => {
